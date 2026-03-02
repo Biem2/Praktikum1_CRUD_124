8bcb06f148365a4f634668a4e19b5b30df005d9b
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2026-03-02 15:23:48 +0700

    Menambhakan UserMapper

---
+++ src/main/java/com/deploy/pratikum1/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.deploy.pratikum1.mapper;
+
+import com.deploy.pratikum1.model.dto.UserDto;
+import com.deploy.pratikum1.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+    UserDto toUserDtoData (User user);
+}
