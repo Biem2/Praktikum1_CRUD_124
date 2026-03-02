9ab29e561dd5f6f15a47d5e616312144246bf61d
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2026-03-02 15:19:17 +0700

    Menambahkan UserRepository

--- src/main/java/com/deploy/pratikum1/repository/UserRepository.java
+++ src/main/java/com/deploy/pratikum1/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.deploy.pratikum1.repository;
 
-public interface UserRepository {
+
+import com.deploy.pratikum1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
